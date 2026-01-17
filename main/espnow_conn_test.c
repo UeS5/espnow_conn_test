e0a5b28670a555fb23aa35a3d10ba0dd1d0ca8ea
UeS5 <umi721@hotmail.com>
2026-01-17 20:38:34 +0100

    made the test

--- main/espnow_conn_test.c
+++ main/espnow_conn_test.c
@@ -20,29 +20,30 @@
 #include <esp_log.h>
 #include <esp_random.h>
 
+#define DATA_SPEED                  0x41
+#define DATA_ENGINE_LOAD            0x04
+
 #define STATUS_ESP_NOW              (1 << 0)
-#define STATUS_ESP_NOW_CONN         (1 << 1)
+
+#define TASK_ESP_NOW_RECEIVE        (1 << 0)
+#define TASK_ESP_NOW_SEND_DATA      (1 << 1)
+#define TASK_DISPLAY_REGISTERS      (1 << 2)
 
 #define BROADCAST_MAC   { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF }
 
 EventGroupHandle_t STATUS_REG;  
+EventGroupHandle_t TASK_REG;
 
-TaskHandle_t vTask_register_mac_hdl;
-TaskHandle_t vTask_pairing_hdl; 
-TaskHandle_t vTask_send_data_hdl;
+TaskHandle_t vTask_start_esp_now_hdl;
 
 QueueHandle_t queue_esp_now_recv;
-QueueHandle_t queue_esp_now_register_mac; 
 
 static const char *TAG_ESP_NOW = "ESP-NOW"; 
 static const char *TAG_MAIN = "MAIN";
 static const char *TAG_RECEIVE = "RECEIVE"; 
-static const char *TAG_REGISTER_MAC = "REGISTER MAC";
-static const char *TAG_PAIRING = "PAIRING"; 
 static const char *TAG_SEND_DATA = "SEND DATA";  
 
 uint8_t global_peer_addr[ESP_NOW_ETH_ALEN] = {0};
-static const uint8_t zero[ESP_NOW_ETH_ALEN] = {0}; 
 
 typedef struct {
 	uint8_t tag;
@@ -84,287 +85,189 @@ void esp_now_recv_cb(const esp_now_recv_info_t *esp_now_info, const uint8_t *dat
 // ESP-NOW RECEIVE: 
 void vTask_esp_now_receive(void *args) {
 
-	uint8_t broadcast_addr[ESP_NOW_ETH_ALEN] = BROADCAST_MAC;	
+	uint8_t broadcast_addr[ESP_NOW_ETH_ALEN] = BROADCAST_MAC;
 	esp_now_data_packet_buff_t pkt;
-
-    ESP_LOGI(TAG_RECEIVE, ">> Info: Receive task has been started.");
+    uint8_t source_addr_buff[ESP_NOW_ETH_ALEN]; 
 	
 	for (;;) {
-		
-		if (xQueueReceive(queue_esp_now_recv, &pkt, portMAX_DELAY) == pdTRUE) {            
-						
-            if (memcmp(pkt.destination_addr, broadcast_addr, ESP_NOW_ETH_ALEN) == 0) {  // if mac_addr = broadcast_addr (FF:FF:FF...)
-				
-                ESP_LOGI(TAG_RECEIVE, ">> Info: Broadcast packet received. Dropping the packet.");
-				free(pkt.pData);	
-
-			} else {
-					
-                // if global_peer_addr is empty, send received mac address to register mac. 
-				if (memcmp(global_peer_addr, zero, ESP_NOW_ETH_ALEN) == 0) {  
-                    ESP_LOGW(TAG_RECEIVE, ">> Info: New ACK packet received. Sending this mac to register task.");  
-                    xQueueOverwrite(queue_esp_now_register_mac, &pkt); 
-                    xTaskNotifyGive(vTask_register_mac_hdl);
-                    free(pkt.pData); 
-                    continue; 
-                }
-                
-                // if source addr is other than global addr drop the packet. 
-                if (memcmp(pkt.source_addr, global_peer_addr, ESP_NOW_ETH_ALEN) != 0) {     
-                    ESP_LOGE(TAG_RECEIVE, ">> Info: Packet from unknown addr has been received. Dropping the packet.");
-                    free(pkt.pData); 
-                    continue; 
-                } 
-
-				ESP_LOGI(TAG_RECEIVE, ">> Info: Data or Ack packet received from registered peer.");
-                free(pkt.pData);
-
-			}	
-		}
-		
-	}
-}
-
-
-
-
-
-// queue_esp_now_recv den gelen mac adreslerini peer olarak ekliyor. 
-void vTask_register_mac (void *args) {
-	uint8_t mac[ESP_NOW_ETH_ALEN] = {0}; 
-	esp_now_peer_info_t newPeer = {0};
-
-    esp_now_data_packet_buff_t pkt; 
-    
-    newPeer.ifidx =WIFI_IF_STA;
-    newPeer.channel = 0;
-	newPeer.encrypt	= false;
-	
-    
-    for (;;) {
-
-	    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+		xEventGroupWaitBits(TASK_REG, TASK_ESP_NOW_RECEIVE, pdFALSE, pdFALSE, portMAX_DELAY);
         
-        if (xQueueReceive(queue_esp_now_recv, &pkt, portMAX_DELAY) == pdPASS) {
-            
-            ESP_LOGI(TAG_REGISTER_MAC, ">> Data in register mac task.");
+        while (xEventGroupGetBits(TASK_REG) & TASK_ESP_NOW_RECEIVE) {
+            if (xQueueReceive(queue_esp_now_recv, &pkt, portMAX_DELAY) == pdTRUE) {
 
-            memcpy(mac, pkt.source_addr, ESP_NOW_ETH_ALEN);
-            memcpy(newPeer.peer_addr, pkt.source_addr, ESP_NOW_ETH_ALEN);
-            esp_err_t err = esp_now_add_peer(&newPeer);
+                free(pkt.pData); // free the heap 
+                memcpy(source_addr_buff, pkt.source_addr, ESP_NOW_ETH_ALEN);
 
-            if (err == ESP_OK) {
-
-                ESP_LOGW(TAG_REGISTER_MAC, ">> Info: A new global address has been added: %02X %02X %02X %02X %02X %02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-                xEventGroupSetBits(STATUS_REG, STATUS_ESP_NOW_CONN);
-                memcpy(global_peer_addr, mac, ESP_NOW_ETH_ALEN);              
-                continue; // start loop again after registering the first mac address.
-            
-            } else {
-                ESP_LOGE(TAG_REGISTER_MAC, ">> Info: Something went wrong with registering."); 
+                if (memcmp(pkt.source_addr, broadcast_addr, ESP_NOW_ETH_ALEN) == 0) {
+                   ESP_LOGI(TAG_RECEIVE, "Data Received from: %02X %02X %02X %02X %02X %02X", source_addr_buff[0] , source_addr_buff[1], source_addr_buff[2], source_addr_buff[3], source_addr_buff[4], source_addr_buff[5]); 
+                }
             }
-	    }
-
-        vTaskDelay(pdMS_TO_TICKS(100));
-    }	 
-    
+    	}
+    }
 }
-
-
 
 
 
 void vTask_start_esp_now(void *pvParameters) {
 
-    ESP_LOGW(TAG_ESP_NOW, "STARTING ESP-NOW...");
-	esp_err_t ret = nvs_flash_init();  
-	ESP_ERROR_CHECK(ret); 								  
-	ESP_ERROR_CHECK(esp_netif_init());
-	ESP_ERROR_CHECK(esp_event_loop_create_default());
-	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); 				 
-	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-	ESP_ERROR_CHECK(esp_wifi_start());
-    ESP_ERROR_CHECK(esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE));
-	esp_err_t init = esp_now_init();
-    esp_err_t err = esp_now_register_recv_cb(esp_now_recv_cb); 
-    
-    vTaskDelay(pdMS_TO_TICKS(50));
-
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG_ESP_NOW, ">> Error: ESP-NOW callback could not be registered!");
-        return; 
-    }
-    
-    if (init != ESP_OK) {
-         ESP_LOGE(TAG_ESP_NOW, ">> Error: ESP-NOW could not be initialised!");
-         return;
-    }
-    
-    ESP_LOGW(TAG_ESP_NOW, ">> Info: ESP-NOW initiated successfully!");
-    xEventGroupSetBits(STATUS_REG, STATUS_ESP_NOW);
-
-	vTaskDelete(NULL); 
-}
-
-
-
-
-
-// esp_now aktiflestirildikten hemen sonra basliyor. amaci broadcast yapip herhangi bir mac adresi almak. ack sinyali geldigi an durur. yerini check conna birakir. 
-void vTask_pairing(void *pvParameters) {
-	uint8_t broadcast_addr[ESP_NOW_ETH_ALEN] = BROADCAST_MAC;
-	esp_now_peer_info_t broadcast = {0};
-	memcpy(broadcast.peer_addr, broadcast_addr, ESP_NOW_ETH_ALEN);
-	broadcast.ifidx = WIFI_IF_STA;
-	broadcast.channel = 0;
-	broadcast.encrypt = false;
-
-	ulTaskNotifyTake(pdTRUE, portMAX_DELAY); 
-
-	esp_err_t err_peer = esp_now_add_peer(&broadcast);
-    
-    if (err_peer != ESP_OK) {
-        ESP_LOGE(TAG_PAIRING, "Could not add the broadcast peer. Returning from the task.");
-        return;
-    }
-    
-	esp_now_data_packet_t broadcast_pkt;
-    broadcast_pkt.tag = 31;
-    broadcast_pkt.data = 0; 
-
-	for (;;) {
-        
-		esp_err_t err = esp_now_send(broadcast_addr, (const uint8_t *)&broadcast_pkt, sizeof(broadcast_pkt));
-
-        if (err == ESP_OK) { 
-            ESP_LOGI(TAG_PAIRING, "Broadcast packet sent successfully.");
-        } else {
-            ESP_LOGE(TAG_PAIRING, "Send error: %s", esp_err_to_name(err));
-        }
-		
-		if (xEventGroupGetBits(STATUS_REG) & STATUS_ESP_NOW_CONN) { 
-            ESP_LOGW(TAG_PAIRING, "EXITING PAIRING PROCESS..."); 
-            break;
-        }  
-			
-		vTaskDelay(pdMS_TO_TICKS(1000));	
-	}		
-
-	vTaskDelete(NULL); 
-}
-
-
-
-
-
-// queue_esp_now dan paketleri alip global_peer_addr ye yollar. 
-void vTask_esp_now_send_data(void *args) {
-    uint8_t randint = 0; 
-	esp_now_data_packet_t pkt; 
-	esp_now_peer_info_t peer;
-	peer.ifidx = WIFI_IF_STA;
-    peer.channel = 0;
-	peer.encrypt = false;	
-	
-    ulTaskNotifyTake(pdTRUE, portMAX_DELAY); 
-    ESP_LOGW(TAG_SEND_DATA, "vTask_esp_now_send_data is triggerred!"); 
-
     for (;;) {
         
-        if (memcmp(global_peer_addr, "\0\0\0\0\0\0", ESP_NOW_ETH_ALEN) == 0) {
-            ESP_LOGE(TAG_SEND_DATA, "Global_peer_addr is EMPTY. Could not send any data."); 
-            vTaskDelay(pdMS_TO_TICKS(200));
-            continue;
+        ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+
+        ESP_LOGW(TAG_ESP_NOW, "STARTING ESP-NOW...");
+    	esp_err_t ret = nvs_flash_init();  
+    	ESP_ERROR_CHECK(ret); 								  
+    	ESP_ERROR_CHECK(esp_netif_init());
+    	ESP_ERROR_CHECK(esp_event_loop_create_default());
+    	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT(); 				 
+    	ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    	ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    	ESP_ERROR_CHECK(esp_wifi_start());
+        ESP_ERROR_CHECK(esp_wifi_set_channel(1, WIFI_SECOND_CHAN_NONE));
+    	esp_err_t init = esp_now_init();
+        esp_err_t err = esp_now_register_recv_cb(esp_now_recv_cb); 
+    
+       vTaskDelay(pdMS_TO_TICKS(50));
+
+       if (err != ESP_OK) {
+           ESP_LOGE(TAG_ESP_NOW, ">> Error: ESP-NOW callback could not be registered!");
+           return; 
+       }
+    
+       if (init != ESP_OK) {
+             ESP_LOGE(TAG_ESP_NOW, ">> Error: ESP-NOW could not be initialised!");
+            return;
         }
+    
+       ESP_LOGW(TAG_ESP_NOW, ">> Info: ESP-NOW initiated successfully!");
+       xEventGroupSetBits(STATUS_REG, STATUS_ESP_NOW);
+    }
+	
+}
 
-        memcpy(peer.peer_addr, global_peer_addr, ESP_NOW_ETH_ALEN);
-        randint = esp_random() & 0xFF;
-        pkt.tag = 1; 
-        pkt.data = randint; 
 
-        esp_err_t err = esp_now_send(peer.peer_addr, (uint8_t *)&pkt, sizeof(pkt));
+
+// Broadcasts random data via ESP-NOW
+void vTask_esp_now_send_data(void *args) {
+    uint8_t randint = 0;
+    bool data_type = 1;
+    uint8_t broadcast_addr[ESP_NOW_ETH_ALEN] = BROADCAST_MAC;
+
+	esp_now_data_packet_t pkt = {0}; 
+	esp_now_peer_info_t peer = {0};
+    memcpy(peer.peer_addr, broadcast_addr, ESP_NOW_ETH_ALEN);
+	peer.ifidx = WIFI_IF_STA;
+    peer.channel = 0;
+	peer.encrypt = false;	 
+
+    for (;;) {
+        xEventGroupWaitBits(TASK_REG, TASK_ESP_NOW_SEND_DATA, pdFALSE, pdFALSE, portMAX_DELAY);
+        
+        while (xEventGroupGetBits(TASK_REG) & TASK_ESP_NOW_SEND_DATA) {
+
+            if (!esp_now_is_peer_exist(peer.peer_addr)) {
+                esp_now_add_peer(&peer);
+            }
+
+            randint = esp_random() & 0xFF;
+            ESP_LOGI(TAG_SEND_DATA, "Random int: %u");
+            pkt.data = randint; 
+            
+            if (data_type) {
+                pkt.tag = DATA_SPEED;
+                data_type = !data_type; 
+            } else {
+                pkt.tag = DATA_ENGINE_LOAD;
+                data_type = !data_type;
+            } 
+
+            esp_err_t err = esp_now_send(peer.peer_addr, (uint8_t *)&pkt, sizeof(pkt));
  
-        if (err == ESP_OK) {
-            ESP_LOGW(TAG_SEND_DATA, ">> Data sent: %u", pkt.data);
-        } else {
-            ESP_LOGE(TAG_SEND_DATA, ">> Error while sending data: %s", esp_err_to_name(err));
-        }
+            if (err == ESP_OK) {
+               ESP_LOGW(TAG_SEND_DATA, ">> Data sent. Tag: %02X Data: %u", pkt.tag, pkt.data);
+            } else {
+               ESP_LOGE(TAG_SEND_DATA, ">> Error while sending data: %s", esp_err_to_name(err));
+            }
 
-        vTaskDelay(pdMS_TO_TICKS(330));
+            vTaskDelay(pdMS_TO_TICKS(330));
+
+        }
     }
 }
 
 
 // Prints event group bits
 void vTask_display_registers(void *args) {
-	
-	uint32_t statusBits = xEventGroupGetBits(STATUS_REG);
+	 
+    uint32_t statusBits; 
+    uint32_t taskBits;
 
 	for (;;) {
-		
-		statusBits = xEventGroupGetBits(STATUS_REG);
-				
-		for (int i = 0; i < 2; i++) {
-			if (statusBits & (1 << i)) {
-				printf("%i", 1);
-				fflush(stdout); 
-			} else {
-				printf("%i", 0);
-				fflush(stdout);
-			}
-		}
+        xEventGroupWaitBits(TASK_REG, TASK_DISPLAY_REGISTERS, pdFALSE, pdFALSE, portMAX_DELAY);
 
-		printf("\n");
-		fflush(stdout);	
-		vTaskDelay(pdMS_TO_TICKS(3000));	
-	}
+        while (xEventGroupGetBits(TASK_REG) & TASK_DISPLAY_REGISTERS) {
+		    statusBits = xEventGroupGetBits(STATUS_REG);
+            taskBits = xEventGroupGetBits(TASK_REG);
+			
+            ESP_LOGI(TAG_ESP_NOW, "Status & Task Registers: ");
+
+		    for (int i = 0; i < 1; i++) {
+    			if (statusBits & (1 << i)) {
+				    printf("%i", 1);
+				    fflush(stdout); 
+			    } else {
+				    printf("%i", 0);
+				    fflush(stdout);
+			    }
+		    }
+
+            printf("\n");
+
+            for (int i = 0; i < 3; i++) {
+                if (taskBits & (1 << i)) {
+                    printf("%i", 1);
+                    fflush(stdout); 
+                } else {
+                    printf("%i", 0);
+                    fflush(stdout);
+                }
+            }
+
+	    	printf("\n");
+	    	fflush(stdout);	
+    		vTaskDelay(pdMS_TO_TICKS(3000));	
+    	}
+    }
 }
 
-
-void vTask_task_manager(void *args) {
-
-
-}
 
 void app_main(void)
 {
 
     STATUS_REG = xEventGroupCreate();
-    queue_esp_now_recv = xQueueCreate(1, sizeof(esp_now_data_packet_buff_t));
-    queue_esp_now_register_mac = xQueueCreate(1, sizeof(esp_now_data_packet_buff_t));
+    TASK_REG = xEventGroupCreate();
 
-    printf("Start of main\n"); 
-    xTaskCreate(vTask_start_esp_now, "ESP-NOW Start", 4096, NULL, 1, NULL);
+    queue_esp_now_recv = xQueueCreate(1, sizeof(esp_now_data_packet_buff_t));
+ 
+    xTaskCreate(vTask_start_esp_now, "ESP-NOW Start", 4096, NULL, 1, &vTask_start_esp_now_hdl);
     xTaskCreate(vTask_display_registers, "Display Registers", 2048, NULL, 1, NULL);
+    xTaskCreate(vTask_esp_now_send_data, "Send Data", 4096, NULL, 1, NULL);
+    xTaskCreate(vTask_esp_now_receive, "Receive", 4096, NULL, 2, NULL);
+
+    // Start
+
+    xTaskNotifyGive(vTask_start_esp_now_hdl);
     
     while ((xEventGroupGetBits(STATUS_REG) & STATUS_ESP_NOW) == 0) {
-        ESP_LOGW(TAG_ESP_NOW, "WAITING FOR ESP-NOW TO START");
+        ESP_LOGW(TAG_MAIN, ">> Info: Waiting for ESP-NOW to start!");
         vTaskDelay(pdMS_TO_TICKS(500)); 
     }
+    
+    ESP_LOGI(TAG_MAIN, ">> Info: Starting to broadcast data"); 
 
-    ESP_LOGW(TAG_ESP_NOW, "STARTING PAIRING MODE AND REGISTER MAC TASK");
-    xTaskCreate(vTask_pairing, "Pairing", 4096, NULL, 1, &vTask_pairing_hdl);
-    xTaskCreate(vTask_register_mac, "Register MAC", 4096, NULL, 3, &vTask_register_mac_hdl);
-    xTaskCreate(vTask_esp_now_receive, "Receive", 4096, NULL, 2, NULL);
-    vTaskDelay(pdMS_TO_TICKS(500));
-    xTaskNotifyGive(vTask_pairing_hdl); 
+    xEventGroupSetBits(TASK_REG, TASK_ESP_NOW_SEND_DATA);
+    xEventGroupSetBits(TASK_REG, TASK_ESP_NOW_RECEIVE);
+    xEventGroupSetBits(TASK_REG, TASK_DISPLAY_REGISTERS);
 
-    uint8_t counter = 0;
-    while ((xEventGroupGetBits(STATUS_REG) & STATUS_ESP_NOW_CONN) == 0) {
-
-        if (counter > 5) {
-            ESP_LOGW(TAG_ESP_NOW, "Waiting for ACK signal");
-            counter = 0;
-        }
-
-        counter++;
-        vTaskDelay(pdMS_TO_TICKS(500)); 
-    }
-
-    ESP_LOGI(TAG_MAIN, ">> Info: Received ACK signal. Stating vTask_esp_now_send_data.");
-    xTaskCreate(vTask_esp_now_send_data, "Send Data", 4096, NULL, 1, &vTask_send_data_hdl);
-    vTaskDelay(pdMS_TO_TICKS(500));
-    xTaskNotifyGive(vTask_send_data_hdl);
-
+    ESP_LOGI(TAG_MAIN, ">> Info: End of Main..."); 
 }
